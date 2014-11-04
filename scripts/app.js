3b89d6b47785addf3824462ac86a1346f92d85d5
youngdze <youngdze94@gmail.com>
2014-11-05 07:28:24 +0800

    0.0.1

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,50 @@
 var todoApp = angular.module('todoApp', [
-    'ngCookies'
+    'ngCookies',
+    'todoApp.todoDirectives'
 ]);
 
 todoApp.controller('todoController',['$scope', '$cookieStore', function ($scope, $cookieStore) {
     $scope.todos = [
         {
             todo: 'Date with GF at 7 pm.',
+            editable: false,
             completed: false
         },
         {
             todo: 'Another date with GF.',
+            editable: false,
             completed: false
         },
         {
             todo: 'Date has been completed.',
+            editable: false,
             completed: true
         }
     ];
 
+    $scope.init = function () {
+        if ($cookieStore.get('todos') != '' && $cookieStore.get('todos') != null) {
+            $scope.todos = $cookieStore.get('todos');
+        }
+    }
+
     $scope.todosLength = function () {
         var j = 0;
-        for (var i = 0; i < $scope.todos.length; i++) {
-            if ($scope.todos[i].completed === false) {
+        angular.forEach($scope.todos, function (todo) {
+            if (todo.completed === false) {
                 j++;
             }
-        }
+        });
         return j;
     };
+
+    $scope.editOn = function (index) {
+        $scope.todos[index].editable = true;
+    }
+
+    $scope.editOff = function (index) {
+        $scope.todos[index].editable = false;
+    }
 
     $scope.addTodo = function () {
         if ($scope.newTodo == undefined || $scope.newTodo == '') {
@@ -38,9 +56,22 @@ todoApp.controller('todoController',['$scope', '$cookieStore', function ($scope,
             });
             $scope.newTodo = null;
         }
+
+        $scope.cookieStore();
     };
 
     $scope.deleteTodo = function (index) {
         $scope.todos.splice(index, 1);
+        $scope.cookieStore();
     };
+
+    $scope.cookieStore = function () {
+        $cookieStore.put('todos', $scope.todos);
+    }
+
+    // $scope.completeAll = function () {
+    //     angular.forEach($scope.todos, function(todo) {
+    //         todo.completed = true;
+    //     });
+    // };
 }]);
