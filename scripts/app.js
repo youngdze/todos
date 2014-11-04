5a84e7e4c0e46f95fc3025d29c5780725f43c568
youngdze <youngdze94@gmail.com>
2014-11-05 04:22:47 +0800

    0.0.0

---
+++ scripts/app.js
@@ -0,0 +1,46 @@
+var todoApp = angular.module('todoApp', [
+    'ngCookies'
+]);
+
+todoApp.controller('todoController',['$scope', '$cookieStore', function ($scope, $cookieStore) {
+    $scope.todos = [
+        {
+            todo: 'A date with GF at 7 pm.',
+            completed: false
+        },
+        {
+            todo: 'Another date with GF.',
+            completed: false
+        },
+        {
+            todo: 'Date has been completed.',
+            completed: true
+        }
+    ];
+
+    $scope.todosLength = function () {
+        var j = 0;
+        for (var i = 0; i < $scope.todos.length; i++) {
+            if ($scope.todos[i].completed === false) {
+                j++;
+            }
+        }
+        return j;
+    };
+
+    $scope.addTodo = function () {
+        if ($scope.newTodo == undefined || $scope.newTodo == '') {
+            return false;
+        } else {
+            $scope.todos.push({
+                todo: $scope.newTodo,
+                completed: false
+            });
+            $scope.newTodo = null;
+        }
+    };
+
+    $scope.deleteTodo = function (index) {
+        $scope.todos.splice(index, 1);
+    };
+}]);
